319fb35426fc88d9fe7fc2bea80afab6e3946168
Afonso Fernandes <81321197+Gongas28@users.noreply.github.com>
2024-01-09 00:06:10 +0000

    Update jogadores.c

--- jogadores.c
+++ jogadores.c
@@ -79,3 +79,146 @@ void login_players(int total_jogadores, JOGADOR jogadores[])
     escolher_tabuleiro(players, qtd, total_jogadores); //Escolher tamanho tabuleiro para jogar
     free(players);
 }
+
+void writePlayers(int total_jogadores, JOGADOR jogadores[], int qtd, JOGADOR players[]) //GRAVA DADOS DOS JOGADORES EM ARQUIVO .TXT
+{
+    FILE *fp = fopen("jogadores.txt", "w");
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o ficheiro!");
+        return;
+    }
+    int i, j;
+    for (i = 0; i < total_jogadores; i++)
+    {
+        int encontrado = 0;
+        for (j = 0; j < qtd; j++)
+	   {
+            if (strcmp(players[j].user, jogadores[i].user) == 0 && strcmp(players[j].password, jogadores[i].password) == 0)
+		  {
+                jogadores[i] = players[j];
+                encontrado = 1; //indicar que o jogador foi encontrado e n?o ? necess?rio escrever novamente os dados no arquivo.
+                break;
+            }
+        }
+        if (encontrado) //? usada para escrever os dados do jogador em jogadores[i] no arquivo de texto no formato
+	   {
+            fprintf(fp, "%s;%s;%s;%d;%d;%s\n", jogadores[i].user, jogadores[i].password, jogadores[i].nacionalidade, jogadores[i].perguntas_respondidas, jogadores[i].idade, jogadores[i].data_jogo);
+        }
+    }
+    fclose(fp);
+}
+
+int readPlayers(JOGADOR jogadores[])
+{
+    FILE *fp = fopen("jogadores.txt", "r");
+    if (fp == NULL)
+    {
+        printf("Erro ao abrir o ficheiro!");
+        return 0;
+    }
+    int i = 0;
+    while (i < MAX_JOGADORES && fscanf(fp, " %[^;];%[^;];%[^;];%d;%d;%[^;\n]\n", jogadores[i].user, jogadores[i].password, jogadores[i].nacionalidade, &jogadores[i].perguntas_respondidas, &jogadores[i].idade, jogadores[i].data_jogo) != EOF)
+    {
+        i++;
+    }
+    fclose(fp);
+    return i;
+}
+
+void show_name(int total_jogadores, JOGADOR jogadores[])
+{
+	JOGADOR aux; //usada para auxiliar na troca de posi??es dos jogadores durante a ordena??o.
+	int i = 0;
+	int j = 0;
+	for(i = 0; i < total_jogadores; i++)
+	{
+    		for(j = 0; j < total_jogadores - 1; j++)
+    {
+        		if(strcmp(jogadores[j].user, jogadores[j + 1].user) > 0)
+	   { // indicar que o jogador atual ? maior que o pr?ximo jogador em ordem alfab?tica, ocorre uma troca de posi??es
+            aux = jogadores[j];
+            jogadores[j] = jogadores[j + 1];
+            jogadores[j + 1] = aux;
+        }
+    }
+}
+for(i = 0; i < total_jogadores; i++){
+    printf("Username: %s\n", jogadores[i].user);
+    printf("Idade: %i\n", jogadores[i].idade);
+    printf("Nacionalidade: %s\n", jogadores[i].nacionalidade);
+    printf("Numero de Respostas: %i\n", jogadores[i].perguntas_respondidas);
+    printf("Data de jogo: %s\n", jogadores[i].data_jogo);
+}
+	system("pause");
+}
+
+void show_age(int total_jogadores, JOGADOR jogadores[])
+{
+JOGADOR aux;
+int i, j;
+for (i = 0; i < total_jogadores; i++) {
+    for (j = 0; j < total_jogadores - 1; j++) {
+        if (jogadores[j].idade > jogadores[j + 1].idade) {
+            aux = jogadores[j];
+            jogadores[j] = jogadores[j + 1];
+            jogadores[j + 1] = aux;
+        }
+    }
+}
+for (i = 0; i < total_jogadores; i++) {
+    printf("Username: %s\n", jogadores[i].user);
+    printf("Idade: %i\n", jogadores[i].idade);
+    printf("Nacionalidade: %s\n", jogadores[i].nacionalidade);
+    printf("Numero de Respostas: %i\n", jogadores[i].perguntas_respondidas);
+    printf("Data de jogo: %s\n", jogadores[i].data_jogo);
+}
+system("pause");
+}
+
+void show_date(int total_jogadores, JOGADOR jogadores[])
+{
+    JOGADOR aux;
+    int i, j;
+    for (i = 0; i < total_jogadores; i++)
+    {
+        for (j = 0; j < total_jogadores - 1; j++)
+	   {
+            struct tm data1, data2;
+            sscanf(jogadores[j].data_jogo, "%d/%d/%d", &data1.tm_mday, &data1.tm_mon, &data1.tm_year);
+            sscanf(jogadores[j + 1].data_jogo, "%d/%d/%d", &data2.tm_mday, &data2.tm_mon, &data2.tm_year);
+            data1.tm_mon -= 1;
+            data1.tm_year -= 1900;
+            data2.tm_mon -= 1;
+            data2.tm_year -= 1900;
+            time_t t1 = mktime(&data1);
+            time_t t2 = mktime(&data2);
+            if (t1 < t2)
+		  {
+                aux = jogadores[j];
+                jogadores[j] = jogadores[j + 1];
+                jogadores[j + 1] = aux;
+            }
+        }
+    }
+    for (i = 0; i < total_jogadores; i++)
+    {
+        printf("Username: %s\n", jogadores[i].user);
+        printf("Idade: %i\n", jogadores[i].idade);
+        printf("Nacionalidade: %s\n", jogadores[i].nacionalidade);
+        printf("Numero de Respostas: %i\n", jogadores[i].perguntas_respondidas);
+        printf("Data de jogo: %s\n", jogadores[i].data_jogo);
+    }
+    system("pause");
+}
+
+char* obterDataAtual()
+{
+    time_t t = time(NULL);
+    struct tm *data = localtime(&t);
+    static char dataStr[11];
+
+    strftime(dataStr, sizeof(dataStr), "%d/%m/%Y", data);
+
+    return dataStr;
+}
